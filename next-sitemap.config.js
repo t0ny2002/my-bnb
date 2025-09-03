03a2de78005473065d28ef4e524fd4ac8a9bd827
Tony Ding <tony.ding2002ding@gmail.com>
2025-09-03 19:02:24 +1000

    added in for google search

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://crownstonequarters.com', // 👈 replace with your domain
+    generateRobotsTxt: true, // also creates robots.txt for you
+    sitemapSize: 5000,
+  };
+  
