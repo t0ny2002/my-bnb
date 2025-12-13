e76d4e4754b24a9834c44a97b21cf259beac3ab9
Tony Ding <tony.ding2002ding@gmail.com>
2025-12-13 12:55:23 +1100

    Changed pic

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -24,7 +24,7 @@ export default function Portfolio() {
       img: '/pyrmont-murray-st-2.jpg',
       alt: 'Modern Pyrmont apartment overlooking Darling Harbour',
       address: '38 York St, Sydney',
-      alias: 'Hidden Sanctuary with Waterside View',
+      alias: 'Urban horizon suite',
       bedrooms: 2,
       bathrooms: 2,
       carports: 0,
